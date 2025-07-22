431b8302a7a505c8bf8a7965a9b439e3d727eff4
CelioB <celiogithubdev@gmail.com>
2025-07-22 18:53:58 -0300

    Add files via upload

---
+++ TiposDeString/TiposDeString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TiposDeString;
+
+class Program
+{
+    static void Main() {
+        char[] letras = new char[] { 'a', 'b', 'c' };
+        char letra = 'a';
+        char numero = '0';
+        char espaco = ' ';
+
+        string texto = "Este curso é muito bom";
+        char primeiraLetraDoTexto = texto[0];
+
+        string meuNome = "           Wellington        ";
+        string meuNomeSemEspaco = meuNome.Trim();
+
+        bool comecaComLetraW = meuNomeSemEspaco.StartsWith("W");
+
+        Console.WriteLine(comecaComLetraW);
+        Console.WriteLine(letra);
+        Console.WriteLine(numero);
+        Console.WriteLine(espaco);
+        Console.WriteLine(texto);
+        Console.WriteLine(primeiraLetraDoTexto);
+        Console.WriteLine(meuNome);
+        Console.WriteLine(meuNomeSemEspaco);
+    }
+}
